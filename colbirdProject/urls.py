67093ccbd74aa345c42e36bad52e21de78f6cd8d
PedroSx29 <pedro.salazar23@inacapmail.cl>
2026-06-14 00:07:31 -0400

    modificacion en todo el proyecto

--- colbirdProject/urls.py
+++ colbirdProject/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from colbirdApp.views import inicio
+from colbirdApp.views import inicio, obtenerLoica
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio, name='inicio'),
-    path('colbird/', include('colbirdApp.urls'))
+    path('obtenerLoica/', obtenerLoica, name='obtenerLoica')
 ] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
